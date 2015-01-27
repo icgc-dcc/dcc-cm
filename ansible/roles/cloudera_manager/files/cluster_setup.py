6d8e2d60b8942c2a225f3e9518a7f559dc9161bf
Sepehr Mavedati <sepehr.mavedati@oicr.on.ca>
2015-01-27 16:02:23 -0500

    Externalize Cloudera Manager settings.

--- ansible/roles/cloudera_manager/files/cluster_setup.py
+++ ansible/roles/cloudera_manager/files/cluster_setup.py
@@ -24,6 +24,11 @@ from time import sleep
 import sys
 import yaml
 import getopt
+import ConfigParser
+
+# Prep for reading config props from external file
+config = ConfigParser.ConfigParser()
+config.read("clouderaconfig.ini")
 
 zookeeper_service_name = "ZOOKEEPER"
 hdfs_service_name = "HDFS"
@@ -38,9 +43,16 @@ service_types_and_names = {
     "MAPREDUCE": mapred_service_name,
     "HBASE": hbase_service_name,
     "OOZIE": oozie_service_name}
-host_list = ['dcc-etl-worker-01', 'dcc-etl-worker-02', 'dcc-etl-worker-03', 'dcc-etl-worker-04']
-etl_main_host = "dcc-etl-main"
-cluster_name = "ETL"
+
+# Cluster name
+cluster_name = config.get("CDH", "cluster.name")
+
+# These include hosts that will run NameNode, DataNode, JobTracker, TaskTracker, etc as well as things like gateways and zookeeper nodes
+host_list = config.get("CDH", "cluster.hosts").split(',')
+
+# This is the main ETL node which will need several clients and gateways.
+main_host = config.get("ETL", "etl.main")
+
 cdh_version = "CDH5"
 cdh_version_number = "5"
 
@@ -50,13 +62,13 @@ cm_password = "admin"
 cm_service_name = "mgmt"
 cm_repo_url = None
 
-reports_manager_host = "dcc-etl-worker-01"
+reports_manager_host = config.get("SERVICES", "reports_manager_host")
 reports_manager_name = "reports_manager"
 reports_manager_username = "rman"
 reports_manager_password = ""
 reports_manager_database_type = "postgresql"
 
-host_username = "ubuntu"
+host_username = config.get("CM", "host_username")
 
 
 def setup_cluster(cm_host, private_key_path):
@@ -246,8 +258,8 @@ def configure_cluster(cm_host, private_key_path):
     private_key = open(private_key_path, 'rb').read()
 
     # install hosts on etl-main
-    cmd = cm.host_install(host_username, [etl_main_host], private_key=private_key, cm_repo_url=cm_repo_url)
-    print "Installing etl_main_host. This will take a while."
+    cmd = cm.host_install(host_username, [main_host], private_key=private_key, cm_repo_url=cm_repo_url)
+    print "Installing main_host. This will take a while."
     while cmd.success is None:
         sleep(30)
         cmd = cmd.fetch()
@@ -260,22 +272,22 @@ def configure_cluster(cm_host, private_key_path):
 
     cluster = api.get_cluster(cluster_name)
 
-    print "adding etl_main_host to cluster"
-    cluster.add_hosts([etl_main_host])
+    print "adding main_host to cluster"
+    cluster.add_hosts([main_host])
 
-    print "adding gateway role to etl_main_host for hbase, hdfs and mapreduce"
+    print "adding gateway role to main_host for hbase, hdfs and mapreduce"
 
     # install HDFS client on etl main node so it can access HDFS
     hdfs_service = cluster.get_service(hdfs_service_name)
-    hdfs_service.create_role("{0}-gw-1".format(hdfs_service_name), "GATEWAY", etl_main_host)
+    hdfs_service.create_role("{0}-gw-1".format(hdfs_service_name), "GATEWAY", main_host)
 
     # install MapReduce client on the etl main node so it can run exporter
     mapred_service = cluster.get_service(mapred_service_name)
-    mapred_service.create_role("{0}-gw-1".format(mapred_service_name), "GATEWAY", etl_main_host)
+    mapred_service.create_role("{0}-gw-1".format(mapred_service_name), "GATEWAY", main_host)
 
     # install HBase client on etl main node
     hbase_service = cluster.get_service(hbase_service_name)
-    hbase_service.create_role("{0}-gw-1".format(hbase_service_name), "GATEWAY", etl_main_host)
+    hbase_service.create_role("{0}-gw-1".format(hbase_service_name), "GATEWAY", main_host)
 
     print "deploying client configurations"
     cluster.deploy_client_config()
@@ -342,6 +354,7 @@ def main(argv):
             private_key_path = arg
         elif opt in ("-h", "--cloudera_manager_host"):
             cm_host = arg
+    
     print "using private_key_path: " + private_key_path
     print "using cloudera manager host: " + cm_host
     setup_cluster(cm_host, private_key_path)
